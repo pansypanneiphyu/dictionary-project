512c714358e52355f92a2082ac2564bad0360028
Pann <pansy.miss@gmail.com>
2022-01-07 14:33:47 +0100

    Added Search input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from "./shecode.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          This project is coded by Pann and{" "}
+          <a href="https://github.com/pansypanneiphyu/dictionary-project">
+            open-sourced on GitHub{" "}
+          </a>
+          and hosted on{" "}
+          <a href="https://peaceful-hypatia-33fa43.netlify.app/">Netlify</a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }

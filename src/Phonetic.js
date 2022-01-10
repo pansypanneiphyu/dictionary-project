6a6fb8012dff2bf6fcf9353254b041dbbbf64bec
Pann <pansy.miss@gmail.com>
2022-01-10 18:33:59 +0100

    Fixed error

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a
+        href={props.phonetic.audio}
+        target="_blank"
+        rel="noreferrer"
+        alt="phonetic-audio"
+      >
         🔊
       </a>
       <span className="text"> {props.phonetic.text}</span>

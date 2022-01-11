6971048d806f05d35b66875a3d4f2c2ddd9af0bd
Pann <pansy.miss@gmail.com>
2022-01-11 17:29:14 +0100

    Styled Meaning.css

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
@@ -8,13 +9,11 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <strong>Example:</strong>
-              <em> {definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">
+              <strong>Example:</strong> {definition.example}
+            </div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}

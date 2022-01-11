6af21c062c256aff58d0f818458170e9d6122c5d
Pann <pansy.miss@gmail.com>
2022-01-11 13:31:28 +0100

    Change the font family

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1 className="text-center">Dictionary App 📖</h1>
+          <h1 className="text-center">Pann's Dictionary App 📖</h1>
         </header>
         <main>
           <Dictionary />
